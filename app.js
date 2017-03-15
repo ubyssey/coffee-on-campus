ac9bac5f51f229da24431ebcfb3488bc59239ad3
Calvin Ye <calvinye900@hotmail.com>
2017-03-14 18:34:55 -0700

    promise for parser

--- app.js
+++ app.js
@@ -45,8 +45,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//var cp = new csvParser();
 var cp = new csvParser();
-
+var cache = cp.parseCSV();
+console.log(cache);
 
 module.exports = app;
 
--- public/parseCSV.js
+++ public/parseCSV.js
@@ -4,20 +4,41 @@
 
 var cache = [];
 
-function ParsingCSV() {
-    const csvFilePath='./public/coffee-on-campus.csv';
-    const csv=require('csvtojson');
+var ParsingCSV = (function (){
 
-    csv().fromFile(csvFilePath)
-        .on('json',function (jsonObj) {
-            console.log(jsonObj);
-            console.log('failed to get to there');
-        // combine csv header row and csv line to a json object
-        // jsonObj.a ==> 1 or 4
-    }).on('done',function(error){
-        console.log('end');
-        console.log('got to here');
-    })
-}
+    ParsingCSV.prototype.parseCSV = function(){
+        return new Promise(function(fulfill,reject){
+            const csvFilePath='./public/coffee-on-campus.csv';
+            const csv=require('csvtojson');
+            csv().fromFile(csvFilePath)
+                .on('json',function (jsonObj) {
+                    cache.push(jsonObj);
+                }).on('done',function(error){
+                    console.log(error);
+                    fulfill(cache);
+                });
+        })
+    }
+
+    
+});
+
+// function ParsingCSV() {
+//
+//     return Promise()
+//     const csvFilePath='./public/coffee-on-campus.csv';
+//     const csv=require('csvtojson');
+//
+//     csv().fromFile(csvFilePath)
+//         .on('json',function (jsonObj) {
+//             // combine csv header row and csv line to a json object
+//             // jsonObj.a ==> 1 or 4
+//             cache.push(jsonObj);
+//             //console.log(cache);
+//     }).on('done',function(error){
+//         console.log("end");
+//         return cache;
+//     });
+// }
 
 module.exports = ParsingCSV;
