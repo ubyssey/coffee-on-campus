f78dbdb852974e277af3c77c7b4f3333ca2bc276
Mei <Mei@Zhaojuns-MacBook-Air.local>
2017-04-21 16:47:02 -0700

    finished querying

--- public/coffee.js
+++ public/coffee.js
@@ -3,114 +3,93 @@
  */
 var coffeeData;
 
-$.getJSON("data.json", function (data) {
-    $("#output").append("<tr><th>Name</th>" +
-        "<th>Location</th>" +
-        "<th>Payment Options</th>" +
-        "<th>Price (medium sized coffee)</th>" +
-        "<th>Quality</th>" +
-        "<th>Service</th>" +
-        "<th>Average Wait Time</th>" +
-        "</tr>");
-
-    for (var index in data) {
-        console.log(data);
-        console.log(data[index].quality);
-        var name = "<tr><td>" + data[index].Name + "</td>";
-        var loc = "<td>" + data[index].Location + "</td>";
-        var po = "<td>" + data[index]["payment options"] + "</td>";
-        var price = "<td>" + data[index]["Price (medium coffee)"] + "</td>";
-        var quality = "<td>" + data[index].quality + "</td>";
-        var service = "<td>" + data[index].service + "</td>";
-        var avgWaitTime = "<td>" + data[index]["waitTime"] + "</td></tr>";
-        $("#output").append(name + loc + po + price + quality + service + avgWaitTime);
-    }
-    coffeeData = data;
+$.getJSON("data.json", function(data){
+   coffeeData = data;
 });
 
+function handleQuery(){
+    var quality = $("#quality").val();
+    var service = $("#service").val();
+    var waitTime = $("#waitTime").val();
+    var price = $("#price").val();
+
+    if (quality == "noPreference" && service == "noPreference" && waitTime == "noPreference" && price == "noPreference") {
+        alert('Please select a Preference');
+        return;
+    }
+    var weightQ = 0;
+    var weightS = 0;
+    var weightT = 0;
+    var weightP = 0;
+
+    if (quality == "goodQuality") {
+        weightQ = 1;
+    } else if (quality == "avgQuality") {
+        weightQ = 0.5;
+    } else if (quality == "poorQuality") {
+        weightQ = 0.1;
+    }
+
+    if (price == "goodPrice") {
+        weightP = 1;
+    } else if (price == "avgPrice") {
+        weightP = 0.5;
+    } else if (price == "poorPrice") {
+        weightP = 0.1;
+    }
+
+    if (service == "goodService") {
+        weightS = 1;
+    } else if (service == "avgService") {
+        weightS = 0.5;
+    } else if (service == "poorService") {
+        weightS = 0.1;
+    }
+
+    if (waitTime == "fast") {
+        weightT = 1;
+    } else if (service == "slow") {
+        weightT = 0.5;
+    }
+
+    for (var index in coffeeData) {
+        coffeeData[index].score = coffeeData[index].price*weightP + coffeeData[index].quality*weightQ + coffeeData[index].service*weightS + coffeeData[index].waitTime*weightT;
+    }
+
+    coffeeData.sort(function(a, b) {
+        return parseFloat(a.score) - parseFloat(b.score);
+    });
+}
+
+function printOutData(dat,size) {
+    $("#output").empty();
+    $("#output").append("<tr><th>Name</th>" +
+        "<th>Location</th>" +
+        "<th>Price (medium sized coffee)</th>" +
+        "<th>Link</th>" +
+        "</tr>");
+
+    for (var i=0; i<size ; i++){
+        var name = "<tr><td>" + dat[i].Name + "</td>";
+        var loc = "<td>" + dat[i].Location + "</td>";
+        var price = "<td>" + dat[i]["Price (medium coffee)"] + "</td>";
+        var link = "<td>" + dat[i].link + "</td></tr>";
+        $("#output").append(name + loc + price + link);
+    }
+}
+
  $("#but1").click(function() {
-     var quality = $("#quality").val();
-     var service = $("#service").val();
-     var waitTime = $("#waitTime").val();
-
-     var matchingPlaces;
-
-     if (quality == "noPreference" && service == "noPreference" && waitTime == "noPreference") {
-         alert('Please select a Preference');
-     }
-     var weightQ = 0;
-     var weightS = 0;
-     var weightT = 0;
-     var weightP = 0;
-
-     if (quality == "goodQuality") {
-         weightQ = 1;
-     } else if (quality == "avgQuality") {
-         weightQ = 0.5;
-     } else if (quality == "poorQuality") {
-         weightQ = 0.1;
-     }
-
-     if (service == "goodService") {
-         weightS = 1;
-     } else if (service == "avgService") {
-         weightS = 0.5;
-     } else if (service == "poorService") {
-         weightS = 0.1;
-     }
-
-     if (waitTime == "fast") {
-         weightT = 1;
-     } else if (service == "slow") {
-         weightT = 0.5;
-     }
-
-     for (var index in coffeeData) {
-        coffeeData[index].score = coffeeData[index].quality*weightQ + coffeeData[index].service*weightS + coffeeData[index].waitTime*weightT;
-     }
-
-     coffeeData.sort(function(a, b) {
-         return parseFloat(a.score) - parseFloat(b.score);
-     });
-
-     console.log(coffeeData[0]);
+     handleQuery();
+     printOutData(coffeeData, 1);
  });
 
 $("#but2").click(function() {
-    var quality = $("#quality").val();
-    var service = $("#service").val();
-    var waitTime = $("#waitTime").val();
-
-    console.log(quality);
-    console.log(service);
-    console.log(waitTime);
-    console.log(coffeeData);
-    var matchingPlaces;
-
-    for (var index in coffeeData) {
-        if (quality != "noPreference" && coffeeData[index].quality == quality ) {
-            matchingPlaces.push(coffeeData[index]);
-        }
-
-    }
+    handleQuery();
+    printOutData(coffeeData, 3);
 });
 
 $("#but3").click(function() {
-    var quality = $("#quality").val();
-    var service = $("#service").val();
-    var waitTime = $("#waitTime").val();
-
-    console.log(quality);
-    console.log(service);
-    console.log(waitTime);
-    console.log(coffeeData);
-    var matchingPlaces;
-
-    for (var index in coffeeData) {
-        if (quality != "noPreference" && coffeeData[index].quality == quality ) {
-            matchingPlaces.push(coffeeData[index]);
-        }
-
-    }
+    handleQuery();
+    printOutData(coffeeData, 5);
 });
 
